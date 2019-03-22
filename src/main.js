615918abfa2df61308252465e9f47e9c9d5ecb8f
liuyanhong-0515 <17319256249@163.com>
2019-03-22 16:36:03 +0800

    配置elementui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 注册ElementUI ·全局组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')

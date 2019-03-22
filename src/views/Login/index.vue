a937abd81589a6b087049f083d020707d7859339
liuyanhong-0515 <17319256249@163.com>
2019-03-22 17:02:06 +0800

    基础登录

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>登录组件</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+
+</style>
